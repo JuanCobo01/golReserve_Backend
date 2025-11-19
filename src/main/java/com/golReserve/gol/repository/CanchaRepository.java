3261ac8656d3f22908c6b19e20dfe52f1a0cd076
Cristian <cristian.ibarra01@uceva.edu.co>
2025-11-19 18:55:08 -0500

    feat: agregar métodos de consulta para validación de disponibilidad en repositories

--- src/main/java/com/golReserve/gol/repository/CanchaRepository.java
+++ src/main/java/com/golReserve/gol/repository/CanchaRepository.java
@@ -1,9 +1,11 @@
 package com.golReserve.gol.repository;
 
 import com.golReserve.gol.entity.Cancha;
+import com.golReserve.gol.entity.Enums.EstadoCancha;
 import com.golReserve.gol.entity.Enums.TipoCancha;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface CanchaRepository extends JpaRepository<Cancha, Long> {
@@ -11,5 +13,6 @@ public interface CanchaRepository extends JpaRepository<Cancha, Long> {
     Optional<Cancha> findById(Long id);
     Optional<Cancha> findByTipoCancha(TipoCancha tipoCancha);
     Optional<Cancha> findByPrecioHora(Double precioHora);
+    List<Cancha> findByEstadoCancha(EstadoCancha estadoCancha);
 
 }
